029f5313611bcf23ee32cecbfb4ba760ad52bf33
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2020-10-18 18:50:26 +0000

    more subjects

--- words.py
+++ words.py
@@ -3,7 +3,10 @@ import random
 class WordGenerator():
   def __init__(self):
     self.__dict_words = {'country': ['Argentina', 'Alemanha', 'Venezuela', 'Colombia', 'Espanha', 'Italia'],
-'food': ['Pizza', 'Lasanha', 'Brigadeiro', 'Feijoada', 'Nhoque', '']}
+'food': ['Pizza', 'Lasanha', 'Brigadeiro', 'Feijoada', 'Nhoque'],
+'technology': ['computador', 'notebook', 'celular', 'tablet'],
+'brand': ['apple', 'samsung', 'dell', 'lg', 'asus', 'positivo']
+}
   
   def get_theme(self):
     theme = random.choice(list(self.__dict_words.keys()))
